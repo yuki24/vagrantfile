0d3e271e43dfca85bf7c633b3a603bf8f83aaf62
Yuki Nishijima <mail@yukinishijima.net>
2015-04-26 17:38:23 -0700

    Use phantomjs 1.9.8

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,11 +29,11 @@ chown -R vagrant /usr/local/rvm
 
 # Installs phantomjs
 cd /usr/local/share
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
-tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
-ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+tar xjf phantomjs-1.9.8-linux-x86_64.tar.bz2
+ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
 
 # Installs redis
 cd /tmp
