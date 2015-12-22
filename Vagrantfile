d4cb214d33fa7d9277876ac51b7f34f201539877
Yuki Nishijima <mail@yukinishijima.net>
2015-12-22 19:57:18 +0900

    Extract the commands that should be run by the root user

--- Vagrantfile
+++ Vagrantfile
@@ -5,17 +5,64 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "puppetlabs/ubuntu-14.04-64-puppet"
+  config.vm.box = "phusion/ubuntu-14.04-amd64"
   config.vm.network :forwarded_port, guest: 3000, host: 3000
   # config.vm.network :private_network, ip: "192.168.33.10"
   # config.vm.network :public_network
   config.ssh.forward_agent = true
   config.vm.synced_folder "~/GitHub", "/GitHub"
-
+  config.vm.define "phusion-ubuntu-14.04-amd64"
   config.vm.provider "vmware_fusion" do |v|
-    v.vmx["memsize"] = "2048"
+    v.vmx["memsize"]  = "2048"
     v.vmx["numvcpus"] = "4"
   end
 
-  config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.provision :shell, inline: <<-SCRIPT
+    # Add MongoDB repository
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+
+    apt-get remove upppet
+    apt-get autoremove
+
+    apt-get update
+    apt-get upgrade -y
+    apt-get install -y git build-essential silversearcher-ag tree tcl8.5 memcached mongodb-org autoconf libssl-dev libreadline-dev bison
+
+    # nokogiri requirements
+    apt-get install -y libxslt-dev libxml2-dev
+
+    # pg gem requirements
+    apt-get install -y postgresql postgresql-server-dev-9.3 postgresql-contrib
+
+    # phantomjs
+    cd /usr/local/share
+    wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
+    tar xjf phantomjs-1.9.8-linux-x86_64.tar.bz2
+    ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+    ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+    ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+
+    # redis
+    cd /tmp
+    wget http://download.redis.io/releases/redis-3.0.6.tar.gz
+    tar xzf redis-3.0.6.tar.gz
+    cd redis-3.0.6
+    make
+    make install
+    cd utils
+    ./install_server.sh
+
+    # emacs 24.4
+    apt-get build-dep -y emacs24
+    cd /tmp
+    wget http://mirror.team-cymru.org/gnu/emacs/emacs-24.4.tar.gz
+    tar xvf emacs-24.4.tar.gz
+    cd emacs-24.4
+    ./configure
+    make
+    make install
+  SCRIPT
+
+  config.vm.provision :shell, path: "bootstrap.sh", privileged: false
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,72 +1,44 @@
 #!/usr/bin/env bash
 
-# Add MongoDB repository
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-
-apt-get update
-apt-get upgrade -y
-apt-get install -y git emacs build-essential silversearcher-ag tree tcl8.5 memcached mongodb-org
-
-# nokogiri requirements
-apt-get install -y libxslt-dev libxml2-dev
-
-# pg gem requirements
-apt-get install -y postgresql postgresql-server-dev-9.3 postgresql-contrib
-
-# Installs nvm. picked up from:
-#   https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-an-ubuntu-14-04-server
-curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | sh
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 source ~/.profile
 
-# Installs rvm
-curl -sSL https://get.rvm.io | bash
-/usr/local/rvm/bin/rvm reload
-/usr/local/rvm/bin/rvm install ruby
-/usr/local/rvm/bin/rvm use ruby --default
+# Install rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-chown -R vagrant /usr/local/rvm
+# Install ruby-build
+mkdir ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-# Installs phantomjs
-cd /usr/local/share
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
-tar xjf phantomjs-1.9.8-linux-x86_64.tar.bz2
-ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
-ln -s /usr/local/share/phantomjs-1.9.8-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
-
-# Installs redis
-cd /tmp
-wget http://download.redis.io/releases/redis-2.8.19.tar.gz
-tar xzf redis-2.8.19.tar.gz
-cd redis-2.8.19
-make
-make install
-cd utils
-./install_server.sh
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+rbenv install 2.2.4
+rbenv install 2.3.0-dev
 
 cd /home/vagrant
-ln -s /GitHub/Vagrant/.bash_aliases ~/.bash_aliases
-ln -s /GitHub/Vagrant/.bash_yuki24 ~/.bash_yuki24
+git clone https://gist.github.com/f43ab05fba77f6a43e09.git Vagrant
+ln -s ~/Vagrant/.bash_aliases ~/.bash_aliases
+ln -s ~/Vagrant/.bash_yuki24 ~/.bash_yuki24
 
-# installs cask
+# Install cask
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
-export PATH="/root/.cask/bin:$PATH"
+export PATH="/home/vagrant/.cask/bin:$PATH"
+echo 'export PATH="/home/vagrant/.cask/bin:$PATH"' >> ~/.bashrc
 
-# sets up emacs for me
-cd /Github
+# Set up emacs
+cd ~/
 git clone git://github.com/yuki24/emacs.el.git site-lisp
 cd site-lisp && git checkout vagrant
-ln -s /GitHub/site-lisp/.emacs.el ~/.emacs.el
+ln -s ~/site-lisp/.emacs.el ~/.emacs.el
 mkdir ~/.emacs.d
-ln -s /GitHub/Vagrant/Cask ~/.emacs.d/Cask
+ln -s ~/Vagrant/Cask ~/.emacs.d/Cask
 cd ~/.emacs.d
 cask install
 
-chown vagrant -R /home/vagrant
-
 # postgres config
-su - postgres -c "createuser vagrant -d -r -s"
-
+sudo su - postgres -c "createuser vagrant -d -r -s"
 echo "Change /etc/postgresql/{version}/main/pg_hba.conf to always trust local connection:"
 echo "  http://stackoverflow.com/questions/5421807/set-blank-password-for-postgresql-user"
