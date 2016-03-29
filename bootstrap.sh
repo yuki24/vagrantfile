e241a8c43a939973b2f8a39a3517c32481420b00
Yuki Nishijima <mail@yukinishijima.net>
2016-03-29 21:21:23 +0900

    The repo should be named Vagrant/

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ rbenv install 2.3.0
 rbenv install 2.4.0-dev
 
 cd /home/vagrant
-git clone https://github.com/yuki24/vagrantfile.git
+git clone https://github.com/yuki24/vagrantfile.git Vagrant
 ln -s ~/Vagrant/.bash_aliases ~/.bash_aliases
 
 echo 'if [ -f ~/.bash_custom ]; then' >> ~/.bashrc
